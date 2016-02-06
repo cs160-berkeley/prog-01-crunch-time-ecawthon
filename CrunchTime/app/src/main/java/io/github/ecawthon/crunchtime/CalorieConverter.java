fbd6b59e5dc28705ff691fb547b1ce7cc2401b94
Eleanor Cawthon <cawthon.e@gmail.com>
2016-02-05 21:15:27 -0800

    Add first pass at input
    UI for user to input a number, select reps or minutes with a radio button, and select an exercise from the drop-down menu

--- CrunchTime/app/src/main/java/io/github/ecawthon/crunchtime/CalorieConverter.java
+++ CrunchTime/app/src/main/java/io/github/ecawthon/crunchtime/CalorieConverter.java
@@ -1,7 +1,9 @@
 package io.github.ecawthon.crunchtime;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CalorieConverter extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class CalorieConverter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calorie_converter);
+
+        final Spinner excChooser = (Spinner) findViewById(R.id.excSpinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item,
+                android.R.id.text1, getResources().getStringArray(R.array.exercises));
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        excChooser.setAdapter(adapter);
+        excChooser.setSelection(0);
+
     }
+
+
 }
